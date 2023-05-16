ae3b505a985daf02db75c7d5170c201fef871ce0
TimilsinaSushil <oct9sushil@gmail.com>
2023-05-16 21:37:53 +0545

    implemented apollo client with react app

---
+++ web-ui/src/App.js
@@ -0,0 +1,17 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Hello from "./components/Hello";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Hello />
+    </ApolloProvider>
+  );
+}
+
+export default App;
