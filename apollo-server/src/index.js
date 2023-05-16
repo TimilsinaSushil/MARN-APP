ac16749365dbdbc3af5b910264fe0e6810357139
TimilsinaSushil <oct9sushil@gmail.com>
2023-05-16 18:54:22 +0545

    implemented basic apollo server

--- apollo-server/src/index.js
+++ apollo-server/src/index.js
@@ -1,1 +1,25 @@
-console.info('hello world!!')
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import gql from "graphql-tag";
+
+// GraphQL Schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// GraphQL Resolvers
+const resolvers = {
+  Query: {
+    hello: () => "Hello from Apollo Server",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers }); // typeDefs are graphql schemas / resolver is like implementation of that schema
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.info(`🚀 Server ready at ${url}`);
